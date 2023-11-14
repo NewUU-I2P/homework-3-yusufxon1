ee13af0407b03fcb7fb1260a83d54b88c4e6db80
yusufxon1 <147371884+yusufxon1@users.noreply.github.com>
2023-11-14 18:53:43 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,7 +1,7 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-i > j && i > k ? result = i:j && j > k ? result = j : rsult = k;
+i > j && i > k ? result = i : j && j > k ? result = j : rsult = k;
     
     return result;
 }
