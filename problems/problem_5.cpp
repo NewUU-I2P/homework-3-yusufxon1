4e7a94ffd4b384f1bce8eefaa1ec944be9a84744
yusufxon1 <147371884+yusufxon1@users.noreply.github.com>
2023-11-14 14:24:44 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,15 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
+   if(operation=='+'){
+      result=x+y;
+   } else if (operation=='-'){
+      result=x-y;
+   } else if (operation=='*'){
+      result==x*y;
+   } else if (operation=='/'){
+      result==x/y;
+   }
 
    return result;
 }
