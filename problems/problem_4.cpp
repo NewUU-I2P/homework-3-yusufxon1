2c9efa84179eeaf26cabe7ea64c12def6131743c
yusufxon1 <147371884+yusufxon1@users.noreply.github.com>
2023-11-14 19:33:30 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -6,11 +6,32 @@ std::string problemSolution4(const std::string &macAddress) {
     std::string result;
     if (macAdress[0] == 'F' and macAdress[1] == 'F') {
         result = "Broadcast";
-    } else if ((int)macAdress[1] % 2 ==0) {
-        result = "Unicast";
     } else {
-        result = "Multicast" ;
+        switch (macAdress[1]){
+            case 'E' :
+            case 'C' :
+            case 'A' :
+            case '8' :
+            case '6' :
+            case '4' :
+            case '2' :
+            case '0' :
+                result = "Unicast";
+                break;
+            case 'F' :
+            case 'D' :
+            case 'B' :
+            case '9' :
+            case '7' :
+            case '5' :
+            case '3' :
+            case '1' :
+                result = "Multicast";
+                break;
+        }
     }
+                
+    
     return result;
 
     // make use of control flow statements
