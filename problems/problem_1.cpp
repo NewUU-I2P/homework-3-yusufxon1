041e003dd464c5e416f42cb360296179e6538ba9
yusufxon1 <147371884+yusufxon1@users.noreply.github.com>
2023-11-14 21:14:15 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,23 +1,15 @@
-//void problemSolution1(float consumed_water) {
-   // float cost;
-#include <iostream>
-using namespace std;
+float problemSolution1(float consumed_water) {
+    float cost;
 
-int main() {
-int consumed_water;
-double cost = 13;
-
-cout << "Enter the water consumption in cubic metres : ";
     // write your code here
-    if (consumed_water <= 30) {
-        cost+ = consumed_water * 0.4;
-    } else if (consumer_water <= 50){
-        cost += 30 * 0.4 + (comsumed_water - 30) * 0.12;
-            } else if (consumed_water <= 60) {
-        cost += 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4;
+    if (consumed_water <=30.0) {
+        cost= 13 + consumed_water * 0.4;
+    } else if (consumer_water <= 50.0){
+        cost = 13 + 30.0 * 0.4 + (comsumed_water - 30.0) * 0.12;
+            } else if (consumed_water <= 60.0) {
+        cost = 13 + 30.0 * 0.4 + 20.0 * 0.12 + (consumed_water - 50.0) * 1.4;
     } else {
-        cost += 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
+        cost = 13 + 30.0 * 0.4 + 20.0 * 0.12 + 10.0 * 1.4 + (consumed_water - 60.0) * 1.5;
     }
-cout << "The total cost is: $" << cost <<endl;
     return cost;
 }
